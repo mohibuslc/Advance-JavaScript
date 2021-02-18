cd137da9ac1e1488d1f92ea1abea0053516f6f9d
mohibuslc <mohib_uslc@outlook.com>
2021-02-18 09:32:58 +0000

    True and False

---
+++ trueandfalse.js
@@ -0,0 +1,20 @@
+let name = 12;
+
+if (name || name == 0){
+
+    console.log("Condition is the True");
+
+}
+else{
+
+    console.log("Condition Is the False");
+
+}
+
+//False Value:                     // object Declear with =  {};
+
+//==================
+//0
+//""
+//undifine
+//null
